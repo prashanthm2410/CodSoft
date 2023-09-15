a3c233bf821ff72715d0fbf09d7c77583f3c46e1
prashanthm2410 <97015668+prashanthm2410@users.noreply.github.com>
2023-09-15 19:47:50 +0530

    Create NumberGame.java

---
+++ Task1-NumberGame/NumberGame.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.println("Welcome to the Number Guessing Game!");
+        boolean rounds;int roundsWon = 0;
+        while(true) {
+            rounds = play(read);
+            if (rounds) {
+                roundsWon++;
+            }
+            System.out.print("Do you want to play again? (yes/no): ");
+            String Again = read.next().toLowerCase();
+            if (!Again.equals("yes")) {
+                break;
+            }
+        }
+        System.out.println("Thanks for playing!\nRounds Won : "+roundsWon);
+        read.close();
+    }
+    public static boolean play(Scanner read) {
+        int max=10,score=0;
+        Random random=new Random();
+        int number=random.nextInt(100) + 1;
+        System.out.println("You can have a Maximum of " + max + " Attempts.");
+        while(score<max) {
+            System.out.print("Enter a Number to Guess : ");
+            try {
+                int num=read.nextInt();
+                int diff=number - num;
+                if(diff==0) {
+                    System.out.println("Congratulations! Guess is correct.");
+                    System.out.println("Number of attempts you have taken is : "+(score + 1));
+                    return true;
+                }else if(diff<0) {
+                    System.out.println("Guess is too high.");
+                }else {
+                    System.out.println("Guess is too low.");
+                }
+                score++;
+            }catch(Exception e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+                read.nextLine();
+            }
+        }
+        if(score>=max) {
+            System.out.println("Maximum attempts reached. The correct number was: " + number);
+        }
+        return false;
+    }
+}
